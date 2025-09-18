70e5b43b3f4a1b6b15b196866dd3e0eae889e0d4
Axel Teo <teojunda@gmail.com>
2025-09-19 02:06:36 +0800

    Add JSON body parser

--- backend/user-service/src/server.ts
+++ backend/user-service/src/server.ts
@@ -2,10 +2,11 @@ import express from 'express';
 import userRoutes from './routes/user-routes.ts';
 
 const app = express();
+app.use(express.json());
 const port = 3000;
 
 // Tell the application to use the homeRoutes for all requests to the root path
-app.use('/', userRoutes);
+app.use('/user', userRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
