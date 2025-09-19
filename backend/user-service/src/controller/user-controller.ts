dd0a4d2021a3223e082e9135d9a022d20a27b0bb
Axel Teo <teojunda@gmail.com>
2025-09-19 21:31:57 +0800

    Add login function

--- backend/user-service/src/controller/user-controller.ts
+++ backend/user-service/src/controller/user-controller.ts
@@ -1,16 +1,50 @@
 import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken';
 import type { Request, Response } from 'express';
 import {
     createUser as _createUser,
     getUserByEmail as _getUserByEmail,
     getUserById as _getUserById,
     getUserByUsername as _getUserByUsername
-} from '../model/user-model.ts'
+} from '../model/user-model.ts';
+import type { User } from '../generated/prisma/index.js';
 
-// Controller function for the hello world
-export const helloWorld = (req: Request, res: Response) => {
-  res.send('Hello, World!');
-};
+export async function loginUser(req: Request, res: Response): Promise<void> {
+  const { email, password } = req.body;
+
+  if (email && password) {
+    try {
+      // Check if user exists
+      const existingUser= await _getUserByUsername(email);
+      if (!existingUser) {
+        res.status(401).json({ error: 'Invalid email address.' });
+        return;
+      }
+
+      // check password matches
+      const isPasswordCorrect = await bcrypt.compare(password, existingUser.passwordHash);
+      if (!isPasswordCorrect) {
+        res.status(401).json({ error: 'Password is wrong.' });
+        return;
+      }
+
+      // generate access token
+      const payload = { id: existingUser.id }
+      const secret = process.env.JWT_SECRET;
+      if (!secret) {
+        throw new Error('JWT_SECRET is not defined in the environment variables');
+      }
+      const accessToken = jwt.sign(payload, secret, { expiresIn: "1d", });
+      res.status(200).json({ data: { accessToken, ...formatUserResponse(existingUser) } });
+      return;
+      
+    } catch (error) {
+      res.status(500).json({ error: 'Internal server error.' });
+    }
+  } else {
+    res.status(400).json({ error: 'Missing email or password.' });
+  }
+}
 
 export async function createUser(req: Request, res: Response): Promise<void> {
   try {
@@ -32,7 +66,7 @@ export async function createUser(req: Request, res: Response): Promise<void> {
       return;
     }
 
-    // Check for duplicate username or password
+    // Check for duplicate username or email
     const existingUserByUsername = await _getUserByUsername(username);
     if (existingUserByUsername) {
       res.status(409).json({ error: 'Username is already taken.' });
@@ -49,15 +83,22 @@ export async function createUser(req: Request, res: Response): Promise<void> {
     const salt = bcrypt.genSaltSync(10);
     const passwordHash = bcrypt.hashSync(password, salt);
 
-    // Call the model function
     const newUser = await _createUser(username, email, passwordHash);
 
-    // Don't return passwordHash in the response
-    const { passwordHash: _, ...safeUser } = newUser;
-
-    res.status(201).json(safeUser);
+    res.status(201).json(formatUserResponse(newUser));
   } catch (error) {
     console.error('Error creating user:', error);
     res.status(500).json({ error: 'Internal server error' });
   }
+}
+
+export function formatUserResponse(user: User) {
+  return {
+    id: user.id,
+    username: user.username,
+    email: user.email,
+    isAdmin: user.isAdmin,
+    createdAt: user.createdAt,
+    updatedAt: user.updatedAt,
+  };
 }
