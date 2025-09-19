8b0ef6846fa27620037e1376b9d928e7b8812519
Axel Teo <teojunda@gmail.com>
2025-09-19 21:29:32 +0800

    Use dotenv to access env variables

--- backend/user-service/src/server.ts
+++ backend/user-service/src/server.ts
@@ -1,13 +1,16 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import userRoutes from './routes/user-routes.ts';
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Tell the application to use the homeRoutes for all requests to the root path
 app.use('/user', userRoutes);
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`User service is running at http://localhost:${port}`);
 });
